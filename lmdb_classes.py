e8ba920b5dc1e13794ffe05844dcd34c3ec4447c
ghostcow <sintzu@gmail.com>
2022-12-22 18:34:09 +0200

    changes: - fixed and updated lmdb_classes.py, not yet tested

--- lmdb_classes.py
+++ lmdb_classes.py
@@ -1,5 +1,4 @@
 import io
-import struct
 from typing import List
 
 import lmdb
@@ -8,38 +7,25 @@ from PIL import Image
 
 
 class ImageRecord(object):
-    def _init_(self, buffer: io.BytesIO, x_offset: int, y_offset: int) -> None:
+    def _init_(self, buffer: io.BytesIO) -> None:
         """
-        This class is used when reading or loading images from the disk. Since we use jpegtran to crop
-        the images and it needs the offsets to be in a 8x16 grid, we have to save a larger crop than intended
-        and crop them again in run-time using this class.
+        This class is used when reading or loading images from the disk.
         """
         assert buffer is not None, "Invalid buffer given!"
         self.image_buf = buffer
-        self.x_offset: int = x_offset
-        self.y_offset: int = y_offset
 
     def get_image(self):
         """
-        :return: a PIL image cropped according to the offsets
+        :return: a PIL image
         """
-        image = Image.open(self.image_buf)
-        width, height = image.size
-        return image.crop((self.x_offset, self.y_offset, width, height))
+        return Image.open(self.image_buf)
 
     def serialize(self):
-        return (
-            struct.pack("<HH", self.x_offset, self.y_offset) + self.image_buf.getvalue()
-        )
+        return self.image_buf.getvalue()
 
     @staticmethod
-    def deserialize(record_bytestring):
-        fmt_str = "<HH"
-        prefix_length = struct.calcsize(fmt_str)
-        x_offset, y_offset = struct.unpack("<HH", record_bytestring[:prefix_length])
-        return ImageRecord(
-            io.BytesIO(record_bytestring[prefix_length:]), x_offset, y_offset
-        )
+    def deserialize(record_bytestring: bytes) -> "ImageRecord":
+        return ImageRecord(buffer=io.BytesIO(record_bytestring))
 
 
 class LmdbReader(object):
@@ -91,15 +77,18 @@ class LmdbWriter(object):
     def close_env(self):
         self.env.close()
 
-    def add_data(self, buffers: List[ImageRecord]):
+    def add_data(self, records: List[ImageRecord]):
         old_count = self.count
-        for buffer in buffers:
+        for record in records:
             key = f"{self.count:09}".encode()
-            self.cache[key] = buffer.serialize()
+            self.cache[key] = record.serialize()
             self.count += 1
 
             if self.count % self.cache_size == self.cache_size - 1:
                 self.write_cache()
+
+        # write remaining samples to lmdb
+        self.write_cache()
 
         if self.count == old_count:
             logger.warning(
@@ -107,21 +96,17 @@ class LmdbWriter(object):
             )
         return
 
-    def write_statistics(self):
-        self.cache[b"num_samples"] = str(self.count).encode()
-        self.write_cache()
-
     def write_cache(self) -> None:
+        # record current number of samples before writing to lmdb
+        self.cache[b"num_samples"] = str(self.count).encode()
+
         with self.env.begin(write=True) as txn:
             for k, v in self.cache.items():
                 o = txn.put(k, v)
                 if o is False:
-                    logger.error("error writing to lmdb or something.")
+                    logger.error("Error writing to lmdb or something.")
         self.clear_cache()
         return
 
     def clear_cache(self) -> None:
-        cache_keys = list(self.cache.keys())
-        for k in cache_keys:
-            del self.cache[k]
-        self.cache = {}
+        self.cache.clear()
